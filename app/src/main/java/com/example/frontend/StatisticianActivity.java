8de8a6eed3e287a360bba265fdd3b9e7aa39a65e
mariabalafouti <mbal2404@gmail.com>
2026-05-11 19:48:49 +0300

    UI: Initial landing screen completed with responsive ConstraintLayout

---
+++ app/src/main/java/com/example/frontend/StatisticianActivity.java
@@ -0,0 +1,13 @@
+package com.example.frontend;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StatisticianActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Προσωρινά βάλε αυτό για να μην σκάει, μέχρι να φτιάξεις το xml της
+        setContentView(R.layout.activity_main);
+    }
+}
